89173db43859c657777c608c850a1fc859a77bd4
Robert Burns <burnsra@gmail.com>
2014-06-11 16:15:28 -0400

    Update to version 4.4.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #     include transmit
 class transmit {
   package { 'Transmit':
-    source   => 'http://www.panic.com/transmit/d/Transmit%204.4.5.zip',
+    source   => 'http://www.panic.com/transmit/d/Transmit%204.4.6.zip',
     provider => 'compressed_app'
   }
 }
